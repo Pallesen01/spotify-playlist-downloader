148d08b0f3b86864e56b6d0d605825c62f2639fe
danie <daniel@raygun.com>
2025-06-05 12:43:10 +1200

    update bat file to be more generic

--- download_playlist.bat
+++ download_playlist.bat
@@ -1,11 +1,13 @@
 @echo off
+:: Create Music directory if it doesn't exist
+if not exist "%USERPROFILE%\Desktop\Music" mkdir "%USERPROFILE%\Desktop\Music"
+
 :: Change file location to where you want the songs to be downloaded
-G:
-cd G:\Music
+cd "%USERPROFILE%\Desktop\Music"
 
 :: Set to url for playlist to be downloaded
-set playlistlink=https://open.spotify.com/user/sparks_of_fire/playlist/5PKXk1rI16nqb0TALQmdqj?si=DId6N6wWQY28D72RUY0tlA
+set playlistlink=https://open.spotify.com/playlist/6kVbhdK2ymPGUUXzXfZXvh?si=6cb919d48ea640bd
 
 echo Downloading Playlist...
-python playlist_downloader.py %playlistlink%
+python "%~dp0playlist_downloader.py" %playlistlink%
 PAUSE
